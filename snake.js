fb452015e0dcc1a9d69e85b71495c463f1586cfa
Dominik Yakoubek <domyak1@nths219.org>
2016-05-07 09:03:28 -0500

    Update snake.js

--- snake.js
+++ snake.js
@@ -1,12 +1,7 @@
 var canvas = document.getElementById("the-game");
 var context = canvas.getContext("2d");
 
-window.addEventListener("keydown", function(e) {
-    // space and arrow keys
-    if ([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
-        e.preventDefault();
-    }
-}, false);
+
 
 game = {
 
