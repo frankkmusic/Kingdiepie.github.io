af25a6870bb3fdca90667aac9c89a7eca366e8da
Dominik Yakoubek <domyak1@nths219.org>
2016-05-07 22:30:29 -0500

    Update pong2.js

--- pong2.js
+++ pong2.js
@@ -8,6 +8,12 @@ addEventListener("keyup", function(e) {
     delete keysDown[e.keyCode];
 }, false);
 
+window.addEventListener("keydown", function(e) {
+    // space and arrow keys
+    if ([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+        e.preventDefault();
+    }
+}, false);
 
 function init() {
 	canvas = document.getElementById("gameCanvas");
